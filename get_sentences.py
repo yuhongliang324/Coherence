b00c873ec93048b055e61a48df2c51a6dd0a2be2
Desai Fan <dfan@andrew.cmu.edu>
2017-04-25 21:43:31 -0400

    Add get_role_codes() and other helper functions.

--- get_sentences.py
+++ get_sentences.py
@@ -104,13 +104,26 @@ def write_all_sents(root_path, out_root, get_sents=get_sents):
 
 
 def get_grid_dict(token_file):
-    # TODO
-    pass
+    token_file_list = token_file.split('/')
+    filename = os.path.splitext(token_file_list[-1])[0] + '.parsed.grid'
+    dataset = '/'.join(token_file_list[-2].split('_'))
+
+    permutation_root = os.path.join(dn, 'permutation')
+
+    grid_file = os.path.join(permutation_root, dataset, filename)
+
+    grid_dict = {}
+    with open(grid_file, 'r') as f:
+        for line in f:
+            content = line.rstrip().split()
+            grid_dict[content[0].lower()] = content[1:]
+
+    return grid_dict
 
 
 def get_role_codes(token_file, pos_file):
     grid_dict = get_grid_dict(token_file)
-    B_set = set(['']) # TODO
+    B_set = set(['be', 'am', 'is', 'are', 'was', 'were', 'being', 'been']) # might add more "stop verbs" into this list!
 
     token_f = open(token_file)
     pos_f = open(pos_file)
@@ -121,10 +134,10 @@ def get_role_codes(token_file, pos_file):
     token_f.close()
     pos_f.close()
 
-    if len(lines) == 0:
+    if len(token_lines) == 0:
         return None
 
-    num_sent = len(lines)
+    num_sent = len(token_lines)
     para_role_codes = []
 
     for i in xrange(num_sent):
@@ -151,12 +164,38 @@ def get_role_codes(token_file, pos_file):
                     else:
                         sent_role_codes.append(role)
                 else:
+                    print 'token_file: %s\npos_file: %s' % (token_file, pos_file)
                     print 'token: %s, pos: %s, startswith N but not in the grid_dict!' % (token, pos)
+                    sent_role_codes.append('X')
             else:
                 sent_role_codes.append('?')
 
         para_role_codes.append(' '.join(sent_role_codes))
     return para_role_codes
+
+
+def write_all_role_codes(root_path, dataset, out_root):
+    if not os.path.isdir(out_root):
+        os.mkdir(out_root)
+    files = os.listdir(root_path)
+    files.sort()
+    for fn in files:
+        if not fn.endswith('.parsed'):
+            continue
+        
+        fn = fn.replace('parsed', 'txt')
+        token_file = os.path.join(preprocessed_root, dataset, fn)
+        pos_file = os.path.join(preprocessed_root, dataset + '_pos', fn)
+
+        role_codes = get_role_codes(token_file, pos_file)
+        # sents = get_sents(os.path.join(root_path, fn))
+        writer = open(os.path.join(out_root, fn), 'w')
+        if role_codes is None:
+            writer.close()
+            continue
+        for role_code in role_codes:
+            writer.write(role_code[:-1] + '.\n')
+        writer.close()
 
 
 
@@ -180,6 +219,15 @@ def test3():
     write_all_sents(earthquake_train_root, earthquake_train_sents_pos_root, get_sents=get_sents)
     write_all_sents(earthquake_test_root, earthquake_test_sents_pos_root, get_sents=get_sents)
 
+def write_role_codes():
+    write_all_role_codes(accident_train_root, 'accident_train', accident_train_sents_root + '_role_code')
+    write_all_role_codes(accident_test_root, 'accident_test', accident_test_sents_root + '_role_code')
+    write_all_role_codes(earthquake_train_root, 'earthquake_train', earthquake_train_sents_root + '_role_code')
+    write_all_role_codes(earthquake_test_root, 'earthquake_test', earthquake_test_sents_root + '_role_code')
+
+
+
 if __name__ == '__main__':
-    test3()
+    # test3()
+    write_role_codes()
 
