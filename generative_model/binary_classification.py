57d91bea1c1c58ce69a8c25e66503f0955768870
yuhongliang324 <yuhongliang324@gmail.com>
2017-05-05 17:31:50 -0400

    add binary classification

---
+++ generative_model/binary_classification.py
@@ -0,0 +1,131 @@
+__author__ = 'yuhongliang324'
+
+import cPickle, os, numpy, theano
+from theano import tensor as T
+from gen import RNN
+
+preprocessed_root = '../preprocessed'
+accident_train_pkl = os.path.join(preprocessed_root, 'accident_train2_final.pkl')
+accident_test_pkl = os.path.join(preprocessed_root, 'accident_test2_final.pkl')
+earthquake_train_pkl = os.path.join(preprocessed_root, 'earthquake_train2_final.pkl')
+earthquake_test_pkl = os.path.join(preprocessed_root, 'earthquake_test2_final.pkl')
+
+
+def load(pkl_file):
+    reader = open(pkl_file)
+    sents, E, xs, ys, discs, disc_labels = cPickle.load(reader)
+    reader.close()
+    lenxs, lenys = [], []
+    maxlen = xs.shape[1]
+    for x in xs:
+        if x[-1] != -1:
+            lenx = maxlen
+        else:
+            lenx = 0
+            while x[lenx] != -1:
+                lenx += 1
+        lenxs.append(lenx)
+        lenys.append(lenx + 1)
+    lenxs = numpy.asarray(lenxs, dtype='int32')
+    lenys = numpy.asarray(lenys, dtype='int32')
+    return sents, E, xs, ys, lenxs, lenys, discs, disc_labels
+
+
+def classify(train_pkl, test_pkl, hidden_dim=256, num_epoch=10):
+    sents_train, E_old, xs_train, ys_train, lenxs_train, lenys_train, discs_train, disc_labels_train = load(train_pkl)
+    sents_test, _, xs_test, ys_test, lenxs_test, lenys_test, discs_test, disc_labels_test = load(test_pkl)
+
+    xs_train = theano.shared(xs_train, borrow=True)
+    ys_train = theano.shared(ys_train, borrow=True)
+    lenxs_train = theano.shared(lenxs_train, borrow=True)
+    lenys_train = theano.shared(lenys_train, borrow=True)
+    xs_test = theano.shared(xs_test, borrow=True)
+    ys_test = theano.shared(ys_test, borrow=True)
+    lenxs_test = theano.shared(lenxs_test, borrow=True)
+    lenys_test = theano.shared(lenys_test, borrow=True)
+
+    E = numpy.zeros((E_old.shape[0] + 1, E_old.shape[1]), dtype=theano.config.floatX)
+    E[: -1] = E_old
+
+    n_class, input_dim = E_old.shape[0], E_old.shape[1]
+    model = RNN(E, input_dim, hidden_dim, n_class)
+    vars = model.build_model()
+
+    x_full, y_full, lenx, leny, is_train, prob = vars['x'], vars['y'], vars['lenx'], vars['leny'], vars['is_train'], vars['prob']
+    att, pred, loss, cost, updates = vars['att'], vars['pred'], vars['loss'], vars['cost'], vars['updates']
+    acc = vars['acc']
+
+    xid, yid = T.iscalar(), T.iscalar()
+    train_model = theano.function(inputs=[xid, yid, is_train],
+                                  outputs=[prob, acc, cost], updates=updates,
+                                  givens={
+                                      x_full: xs_train[xid], y_full: ys_train[yid],
+                                      lenx: lenxs_train[xid], leny: lenys_train[yid]
+                                  },
+                                  on_unused_input='ignore', mode='FAST_RUN')
+    test_model = theano.function(inputs=[xid, yid, is_train],
+                                 outputs=[prob, acc, cost],
+                                 givens={
+                                     x_full: xs_test[xid], y_full: ys_test[yid],
+                                     lenx: lenxs_test[xid], leny: lenys_test[yid]
+                                 },
+                                 on_unused_input='ignore', mode='FAST_RUN')
+
+    report_iter = 100
+
+    for epoch_index in xrange(num_epoch):
+        iter_index = 0
+        prob_report, acc_report, cost_report = 0., 0., 0.
+        prob_epoch, acc_epoch, cost_epoch = 0., 0., 0.
+        for disc, label in zip(discs_train, disc_labels_train):
+            if label == 0:
+                continue
+            n_sent = len(disc)
+            for i in xrange(n_sent - 1):
+                xid, yid = disc[i], disc[i + 1]
+
+                prob, acc, cost = train_model(xid, yid, 1)
+                prob_report += prob
+                acc_report += acc
+                cost_report += cost
+
+                prob_epoch += prob
+                acc_epoch += acc
+                cost_epoch += cost
+
+                iter_index += 1
+                if iter_index % report_iter == 0:
+                    prob_report /= report_iter
+                    acc_report /= report_iter
+                    cost_report /= report_iter
+                    print '\tEpoch = %d, iter = %d, prob = %.5f, acc = %.5f, cost = %.4f'\
+                          % (epoch_index + 1, iter_index, prob_report, acc_report, cost_report)
+                    prob_report, acc_report, cost_report = 0., 0., 0.
+        prob_epoch /= iter_index
+        acc_epoch /= iter_index
+        cost_epoch /= iter_index
+        print 'Training epoch = %d, prob = %.5f, acc = %.5f, cost = %.4f'\
+              % (epoch_index + 1, prob_epoch, acc_epoch, cost_epoch)
+
+
+def validate(test_model, discs_test, discs_labels_test):
+    iter_index = 0
+    prob_epoch, acc_epoch, cost_epoch = 0., 0., 0.
+    for disc, label in zip(discs_test, discs_labels_test):
+        if label == 0:
+            continue
+        n_sent = len(disc)
+        for i in xrange(n_sent - 1):
+            xid, yid = disc[i], disc[i + 1]
+            prob, acc, cost = test_model(xid, yid, 0)
+            prob_epoch += prob
+            acc_epoch += acc
+            cost_epoch += cost
+            iter_index += 1
+
+    prob_epoch /= iter_index
+    acc_epoch /= iter_index
+    cost_epoch /= iter_index
+    print 'Test prob = %.5f, acc = %.5f, cost = %.4f'\
+          % (prob_epoch, acc_epoch, cost_epoch)
+
---
+++ generative_model/gen.py
@@ -0,0 +1,142 @@
+__author__ = 'yuhongliang324'
+
+import sys
+sys.path.append('..')
+import theano
+import theano.tensor as T
+import numpy
+from theano.tensor.shared_randomstreams import RandomStreams
+from lstm.theano_utils import Adam, Adam2, RMSprop, SGD, dropout
+
+
+class RNN(object):
+    def __init__(self, E, input_dim, hidden_dim, n_class, n_roles=0, hidden_dim_role=50, lamb=0., update='adam2',
+                 drop=0.2):
+        self.E = theano.shared(E, borrow=True)
+        self.input_dim, self.hidden_dim = input_dim, hidden_dim
+        self.n_class = n_class
+        self.lamb = lamb
+        self.drop = drop
+        self.update = update
+        self.rng = numpy.random.RandomState(1234)
+        theano_seed = numpy.random.randint(2 ** 30)
+        self.theano_rng = RandomStreams(theano_seed)
+
+        self.W_enc_i, self.b_enc_i, self.U_enc_i, \
+        self.W_enc_f, self.b_enc_f, self.U_enc_f, \
+        self.W_enc_o, self.b_enc_o, self.U_enc_o, \
+        self.W_enc_c, self.b_enc_c, self.U_enc_c = self.create_lstm_para()
+        self.theta = [self.W_enc_i, self.b_enc_i, self.U_enc_i,
+                      self.W_enc_f, self.b_enc_f, self.U_enc_f,
+                      self.W_enc_o, self.b_enc_o, self.U_enc_o,
+                      self.W_enc_c, self.b_enc_c, self.U_enc_c]
+
+        self.W_dec_i, self.b_dec_i, self.U_dec_i, \
+        self.W_dec_f, self.b_dec_f, self.U_dec_f, \
+        self.W_dec_o, self.b_dec_o, self.U_dec_o, \
+        self.W_dec_c, self.b_dec_c, self.U_dec_c = self.create_lstm_para()
+        self.theta += [self.W_dec_i, self.b_dec_i, self.U_dec_i,
+                       self.W_dec_f, self.b_dec_f, self.U_dec_f,
+                       self.W_dec_o, self.b_dec_o, self.U_dec_o,
+                       self.W_dec_c, self.b_dec_c, self.U_dec_c]
+        '''
+        if n_roles > 0:
+            self.R, _ = self.init_para(n_roles, hidden_dim_role)
+            self.theta.append(self.R)'''
+        self.W, self.b = self.init_para(self.hidden_dim, self.n_class)
+        self.theta += [self.W, self.b]
+
+        print 'lambda =', self.lamb, 'last =', '#class =', self.n_class, 'drop =', self.drop, 'update =', self.update
+
+        if self.update == 'adam':
+            self.optimize = Adam
+        elif self.update == 'adam2':
+            self.optimize = Adam2
+        elif self.update == 'rmsprop':
+            self.optimize = RMSprop
+        else:
+            self.optimize = SGD
+
+    def create_lstm_para(self):
+        W_i, b_i = self.init_para(self.input_dim, self.hidden_dim)
+        U_i, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+        W_f, b_f = self.init_para(self.input_dim, self.hidden_dim)
+        U_f, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+        W_o, b_o = self.init_para(self.input_dim, self.hidden_dim)
+        U_o, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+        W_c, b_c = self.init_para(self.input_dim, self.hidden_dim)
+        U_c, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+        return [W_i, b_i, U_i, W_f, b_f, U_f, W_o, b_o, U_o, W_c, b_c, U_c]
+
+    def init_para(self, d1, d2):
+        W_values = numpy.asarray(self.rng.uniform(
+            low=-numpy.sqrt(6. / float(d1 + d2)), high=numpy.sqrt(6. / float(d1 + d2)), size=(d1, d2)),
+            dtype=theano.config.floatX)
+        W = theano.shared(value=W_values, borrow=True)
+        b_values = numpy.zeros((d2,), dtype=theano.config.floatX)
+        b = theano.shared(value=b_values, borrow=True)
+        return W, b
+
+    def l2(self):
+        l2 = self.lamb * T.sum([T.sum(p ** 2) for p in self.theta])
+        return l2
+
+    def encode_step(self, X_t, C_tm1, H_tm1):
+        i_t = T.nnet.sigmoid(T.dot(X_t, self.W_enc_i) + T.dot(H_tm1, self.U_enc_i) + self.b_enc_i)
+        f_t = T.nnet.sigmoid(T.dot(X_t, self.W_enc_f) + T.dot(H_tm1, self.U_enc_f) + self.b_enc_f)
+        o_t = T.nnet.sigmoid(T.dot(X_t, self.W_enc_o) + T.dot(H_tm1, self.U_enc_o) + self.b_enc_o)
+        C_t = T.tanh(T.dot(X_t, self.W_enc_c) + T.dot(H_tm1, self.U_enc_c) + self.b_enc_c)
+        C_t = i_t * C_t + f_t * C_tm1
+        H_t = o_t * T.tanh(C_t)
+        return C_t, H_t
+
+    def decode_step(self, Yc_t, C_tm1, H_tm1):
+        i_t = T.nnet.sigmoid(T.dot(Yc_t, self.W_dec_i) + T.dot(H_tm1, self.U_dec_i) + self.b_dec_i)
+        f_t = T.nnet.sigmoid(T.dot(Yc_t, self.W_dec_f) + T.dot(H_tm1, self.U_dec_f) + self.b_dec_f)
+        o_t = T.nnet.sigmoid(T.dot(Yc_t, self.W_dec_o) + T.dot(H_tm1, self.U_dec_o) + self.b_dec_o)
+        C_t = T.tanh(T.dot(Yc_t, self.W_dec_c) + T.dot(H_tm1, self.U_dec_c) + self.b_dec_c)
+        C_t = i_t * C_t + f_t * C_tm1
+        H_t = o_t * T.tanh(C_t)  # (hid_dim,)
+        return C_t, H_t
+
+    def build_model(self):
+        x_full = T.imatrix()  # (max_len,)
+        y_full = T.imatrix()  # (max_len,)
+        lenx = T.iscalar()
+        leny = T.iscalar()
+        x = x_full[: lenx]  # (lenx,)
+        y = y_full[: leny]  # (leny,)
+        X = self.E[x]  # (lenx, 300)
+        Yc = self.E[y[:-1]]  # (leny - 1, 300)
+        yn = y[1:] # (leny - 1,)
+
+        [_, H_enc], _ = theano.scan(self.encode_step, sequences=X,
+                                    outputs_info=[T.zeros((self.hidden_dim,),
+                                                          dtype=theano.config.floatX),
+                                                  T.zeros((self.hidden_dim,),
+                                                          dtype=theano.config.floatX)])
+        rep_enc = H_enc[-1]  # (hidden_dim)
+        # H_dec: (leny - 1, hid_dim)
+        [_, H_dec], _ = theano.scan(self.decode_step, sequences=Yc,
+                                    outputs_info=[T.zeros((self.hidden_dim,),
+                                                          dtype=theano.config.floatX),
+                                                  rep_enc])
+
+        is_train = T.iscalar('is_train')
+
+        rep_dec = T.dot(H_dec, self.W) + self.b  # (leny - 1, n_class)
+        rep_dec = dropout(rep_dec, is_train, drop_ratio=self.drop)  # (leny - 1, n_class)
+
+        prob = T.nnet.softmax(rep_dec)
+        pred = T.argmax(prob, axis=-1)
+
+        acc = T.mean(T.eq(pred, yn))
+        loss = T.mean(T.nnet.categorical_crossentropy(prob, yn))
+        cost = loss + self.l2()
+
+        updates = self.optimize(cost, self.theta)
+
+        ret = {'x': x_full, 'y': y_full, 'lenx': lenx, 'leny': leny, 'is_train': is_train, 'prob': prob,
+               'att': None, 'pred': pred, 'loss': loss, 'cost': cost, 'updates': updates,
+               'acc': acc}
+        return ret
